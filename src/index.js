cda3eccfde3bed9687c0cad4b49aeae6479d2410
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-08-22 13:15:42 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  return str.split('').every((bracket) => {
+    if (isBracketSame(bracket, bracketsConfig)) {
+      stack[stack.length - 1] === bracket ? stack.pop() : stack.push(bracket);
+      return true;
+    }
+    if (isBracketOpening(bracket, bracketsConfig)) {
+      stack.push(bracket);
+      return true;
+    }
+    if (stack[stack.length - 1] === getPairedBracket(bracket, bracketsConfig)) {
+      stack.pop();
+      return true;
+    }
+    return false;
+  }) && stack.length === 0;
+}
+
+const isBracketOpening = (bracket, bracketsConfig) => bracketsConfig
+  .some(([opening]) => opening === bracket);
+
+const isBracketSame = (bracket, bracketsConfig) => bracketsConfig
+  .some(([opening, closing]) => opening === bracket && closing === bracket);
+
+const getPairedBracket = (closingBracket, bracketsConfig) => {
+  const [opening] = bracketsConfig.find(([, closing]) => closing === closingBracket);
+  return opening;
 }
